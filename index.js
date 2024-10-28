4cd32b8c82b3ae54b4916ad322d591653fcbbfbf
MdRashid838 <160717010+MdRashid838@users.noreply.github.com>
2024-10-28 13:08:02 +0530

    add

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ app.get('/', function(req, resp){
         resp.render("index" ,{files: files});
     })
 })
+
+app.get('/file/:filename', function(req, resp){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8", function(err, filedata){
+    resp.render('show' ,{filename: req.params.filename ,filedata: filedata});
+    })
+})
+
+app.get('/edit/:filename', function(req, resp){
+   resp.render('edit',{filename: req.params.filename})
+})
+
+app.post('/edit', function(req, resp){
+    fs.rename(`/files/${req.body.previous}`, `/files/${req.body.new}`, function(err){
+        resp.redirect('/')
+    })
+})
+
+
 app.post('/create', function(req, resp){
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,function(err){
         resp.redirect('/');
